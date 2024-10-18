a1e99bb567b46c33782b498d162253c40edd666a
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-10-18 11:29:47 +0600

    adding unordered map and multimap

---
+++ 13_unordered_maps.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //all are same without time complexity and valid keys.
+    unordered_map<int,string> m; // unordered_map<pair<int,int>,string> m; not use.
+    m[3]="abc";//O(1); 
+
+    //multimap
+    multimap<int,vector<int>> ml;//multipule value exit.
+}

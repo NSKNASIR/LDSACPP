6dc30c3dd1669434d2dcf30ef91d90eedba95452
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-10-22 23:56:58 +0600

    binary search

---
+++ 23_binary_search.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;cin>>n;
+    vector<int> v(n);
+    for(auto & ele:v) cin>>ele;
+
+    int fast,last,mid,key;
+    cin>>key;
+    fast = 0;last = n-1;
+    bool ans = false;
+    while(fast<=last){
+        mid = (fast+last)/2;
+        if(v[mid] == key){
+            ans = true;
+            break;
+        }else if(v[mid] < key) fast = mid+1;
+        else last = mid -1;
+    }
+    if(ans) cout<<"value is found at index "<<mid;
+    else cout<<"value is not found";
+}

68e02aaaa3277bad0fdb8d3b9460a1a79fc5700c
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-10-26 23:43:16 +0600

    26-10-24

--- 20_stl_inbuilt_algorithms.cpp
+++ 20_stl_inbuilt_algorithms.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    // int n;cin>>n;
-    // vector<int> v(n);
-    // for(int &i:v) cin>>i;
-    // int min = *min_element(v.begin(),v.end());
-    // int max = *max_element(v.begin(),v.end());
-    // int sum = accumulate(v.begin(),v.end(),0);//here 0 is initial value
-    // int ct = count(v.begin(),v.end(),3);//count value 3 in the vector v.
-    // int element = *find(v.begin(),v.end(),5);
-    // cout<<min<<" "<<max<<" "<<sum<<" "<<ct<<" "<<element<<endl;
-    // reverse(v.begin(),v.end());
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int &i:v) cin>>i;
+    int min = *min_element(v.begin(),v.end());
+    int max = *max_element(v.begin(),v.end());
+    int sum = accumulate(v.begin(),v.end(),0);//here 0 is initial value
+    int ct = count(v.begin(),v.end(),3);//count value 3 in the vector v.
+    int element = *find(v.begin(),v.end(),5);
+    cout<<min<<" "<<max<<" "<<sum<<" "<<ct<<" "<<element<<endl;
+    reverse(v.begin(),v.end());
 
     //lambda function
-    // cout<<[](int x,int y){return x+y;}(2,3);
+    cout<<[](int x,int y){return x+y;}(2,3);
     auto sum = [](int x,int y){
         cout<<"This is lambda function"<<endl;
         return x+y;
     };
-    // cout<<sum(2,3);
+    cout<<sum(2,3);
 
     vector<int> v = {2,3,4};
     

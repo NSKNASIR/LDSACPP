14f82126c3d15917b7d12b5713ef4bf99ecfb1ec
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-10-31 21:13:11 +0600

    31-10-24

--- 27_binary_number_and_bits.cpp
+++ 27_binary_number_and_bits.cpp
@@ -1,9 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+void printBinary(int n){
+    for(int i=10;i>=0;--i) cout<<((n>>i) & 1);
+    cout<<endl;
+}
 int  main(){
     cout<<INT_MAX<<endl;
     int a = (1LL<<31)-1;
     cout<<a<<endl;
     unsigned int b = (1LL<<32)-1;
     cout<<b<<endl;
+    printBinary(16);
+
+    //set bit and unset bit
+    cout<<(16 & (1<<2))<<endl; // if answer is 1 then it is set bit else answer is 0 its unset bit.
+
+    //bit set
+    printBinary(3 | (1<<3));
+
+    //bit unset
+    printBinary(7 & ~(1<<2));
+
+    //toggle
+    printBinary(7 ^ (1<<1));
+
+    //count 1 in a binary number
+    int ct=0;
+    for(int i=31;i>=0;--i) if((15 & (1<<i)) !=0) ct++;
+    cout<<ct<<endl;
 }

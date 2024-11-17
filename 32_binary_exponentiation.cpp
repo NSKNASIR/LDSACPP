2c81f16c6b57236b03c92b7f42bd5ccadf078e56
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-11-17 20:00:35 +0600

    binary exponentiation using recursion and iterative method

--- 32_binary_exponentiation.cpp
+++ 32_binary_exponentiation.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int pw(int n, int m){
+//Recursive Method
+int pwRec(int n, int m){
     if(m == 0) return 1;
-    int res = pw(n,m/2);
+    int res = pwRec(n,m/2);
     if(m&1){
         return n * res * res;
     }else{
@@ -11,7 +12,18 @@ int pw(int n, int m){
     }
 }
 
+//Iterative Method
+int pwIt(int n,int m){
+    int ans = 1;
+    while(m){
+        if(m&1) ans *= n;
+        n *=n;
+        m >>=1;
+    }
+    return ans;
+}
+
 int main(){
     int n,m;cin>>n>>m;
-    cout<<pw(n,m);
+    cout<<pwIt(n,m);
 }

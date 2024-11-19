2146ee781938ef86a39e9b12ad35563d9d34a527
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-11-19 11:28:43 +0600

    19/11/24

---
+++ 35_sieve_algo_prime.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    vector<bool> arr(n+1,true);
+
+    for(int i=2;i<=sqrt(n);++i){
+        for(int j=i*i;j<=n;j+=i){
+            arr[j]=false;
+        }
+    }
+    for(int i=2;i<=n;i++){
+        if(arr[i]) cout<<i<<endl;
+    }
+}

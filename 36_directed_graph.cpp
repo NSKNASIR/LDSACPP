df92cf7385c13dc1bb5138438a866b88a432ff6d
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-11-20 23:38:44 +0600

    20/11/24

---
+++ 36_directed_graph.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e3;
+int graph[N][N];
+
+vector<int> Graph[N];
+
+int main(){
+    int n;cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     int a,b;cin>>a>>b;
+    //     graph[a][b]=1;
+    // }
+    // cout<<endl;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n;j++){
+    //         if(graph[i][j]) cout<<i<<" "<<j<<endl;
+    //     }
+    // }
+
+    for(int i=1;i<=n;++i){
+        int a,b;cin>>a>>b;
+        Graph[a].push_back(b);
+
+    }
+    for(auto ele:Graph){
+        for(auto &i :ele){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+}

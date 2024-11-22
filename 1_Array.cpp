76d6f164347b3d7a0b561e9f5c78cbe11963c1a0
nsk <nsknasiruddinbhuiyan@gmail.com>
2024-11-22 23:25:51 +0600

    22/11/24

--- 1_Array.cpp
+++ 1_Array.cpp
@@ -5,9 +5,9 @@ const int n = 1e7; //Global array size = n <= 1e7 . Ita using Data segmentation.
 int arr[n];
 
 int main(){
-    // int n;
-    // n=1e7; Local array size = n <= 1e5 . Its using stack.
-    // int arr[n];
+    int n;
+    n=1e7; //Local array size = n <= 1e5 . Its using stack.
+    int arr[n];
     arr[n-1]=7;
     cout<<arr[n-1];
 }
